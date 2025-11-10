3cce8997a2bb0a468ce17967d6170fcc3ad6faf6
Soheil Assadi <suheyl.asadi@gmail.com>
2025-11-11 02:25:13 +0300

    3rd project uploaded.

---
+++ random-quote-generator/index.js
@@ -0,0 +1,24 @@
+const quotes = [
+  'The only way to do great work is to love what you do.',
+  "Life is what happens when you're busy making other plans.",
+  'The future belongs to those who believe in the beauty of their dreams.',
+  'It does not matter how slowly you go as long as you do not stop.',
+  "In the end, it's not the years in your life that count. It's the life in your years.",
+  'The purpose of our lives is to be happy.',
+  'Get busy living or get busy dying.',
+  'You only live once, but if you do it right, once is enough.',
+  "Many of life's failures are people who did not realize how close they were to success when they gave up.",
+  'If you want to live a happy life, tie it to a goal, not to people or things.',
+];
+
+const pElement = document.querySelector('.js-quote-paragraph');
+const clickElement = document.querySelector('.js-click-button');
+
+function generateQuote() {
+  const randomNum = Math.floor(Math.random() * 10);
+  pElement.innerHTML = quotes[randomNum];
+}
+
+clickElement.addEventListener('click', () => {
+    generateQuote()
+});
